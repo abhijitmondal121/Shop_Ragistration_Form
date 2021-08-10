0980c4d9d447b60724975797941780f16e62a47c
ABHIJIT MONDAL <70851067+abhijitmondal121@users.noreply.github.com>
2021-08-10 22:35:48 +0530

    Update Page3.php

--- 3/Page3.php
+++ 3/Page3.php
@@ -45,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-    <!-- jQuery library -->
+    <!-- jQuery-library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!-- Latest compiled JavaScript -->
